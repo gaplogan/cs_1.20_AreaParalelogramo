80f01fcf6c2937d43f0ba993ff41ef2183fbcc2a
Gabriel <gaplogan@gmail.com>
2021-03-02 15:00:14 -0300

    2021.03.02.15.00

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cs_1._20_AreaParalelogramo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaração das variáveis do programa
+            double b, h, area;
+
+            // Entrada de dados
+            Console.Write("Digite o valor da base: ");
+            b = double.Parse(Console.ReadLine());
+            b = Math.Abs(b); // Garante base positiva
+            Console.Write("Digite o valor da altura: ");
+            h = double.Parse(Console.ReadLine());
+            h = Math.Abs(h); // Garante base positiva
+
+            // Processamento de dados
+            area = b * h;
+
+            // Saída de dados
+            Console.WriteLine($"Área do paralelogramo: {area}");
+        }
+    }
+}
